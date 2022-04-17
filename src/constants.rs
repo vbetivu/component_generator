9b408d458a60c72baaae618c9b1bb0c0a625d444
Vladislav Betivu <vlad.betivu@gmail.com>
2022-04-17 11:47:08 +0300

    Add template folder scan

---
+++ src/constants.rs
@@ -0,0 +1,3 @@
+pub const TEMPLATE_FOLDER: &str = "./fg_template";
+
+pub const TRANSFORM_KEBAB_CASE: &str = "transform:kebab-case";
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 pub mod config;
+pub mod constants;
+
+use std::fs;
+use std::io;
+use std::path::{Path, PathBuf};
 
 use config::Config;
+use constants::TEMPLATE_FOLDER;
 
 struct Template {
     config: Config,
@@ -11,17 +17,45 @@ impl Template {
         Template { config }
     }
 
-    fn generate(&mut self) -> Result<(), &'static str> {
-        println!(
-            "{} {} {}",
-            self.config.dir, self.config.generate_all, self.config.component_name
-        );
+    fn generate(&mut self) -> Result<(), String> {
+        println!("{:#?}", self.config);
+
+        let dir_path = Path::new(TEMPLATE_FOLDER);
+
+        if !dir_path.is_dir() {
+            return Result::Err(format!(
+                "Template folder is missing. Please create \"{}\".",
+                TEMPLATE_FOLDER
+            ));
+        }
+
+        let templates = read_templates(dir_path).unwrap();
+        println!("{:#?}", templates);
 
         return Result::Ok(());
     }
 }
 
-pub fn run(config: Config) -> Result<(), &'static str> {
+fn read_templates(path: &Path) -> Result<Vec<PathBuf>, io::Error> {
+    let mut templates: Vec<PathBuf> = Vec::new();
+
+    for template_entry in fs::read_dir(path)? {
+        let template_entry = template_entry?;
+        let path = template_entry.path();
+
+        if !path.is_dir() {
+            templates.push(path);
+        }
+    }
+
+    if templates.len() != 0 {
+        Result::Ok(templates)
+    } else {
+        Result::Err(io::Error::new(io::ErrorKind::Other, "Empty directory."))
+    }
+}
+
+pub fn run(config: Config) -> Result<(), String> {
     let mut text = Template::new(config);
 
     text.generate()?;
