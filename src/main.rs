861706c21ef66aec4d754d8bed28571bf30f3ebb
Vladislav Betivu <vlad.betivu@gmail.com>
2022-05-12 16:32:26 +0300

    Init project

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use std::{env, process};
-
-use component_generator::config::Config;
-
 fn main() {
-    let cli_config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Error parsing the arguments: {}", err);
-        process::exit(1);
-    });
-
-    if let Err(e) = component_generator::run(cli_config) {
-        eprintln!("Generation error: {}", e);
-
-        process::exit(1);
-    }
+    println!("Hello, world!");
 }
